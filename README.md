d78da593ee52c5cecb4843edb635a37d3f8433b3
federico <feluc@msft.com>
2024-04-05 10:46:17 +0200

    added files needed to advsec

--- README.md
+++ README.md
@@ -168,3 +168,5 @@ We have some great contributions from the community, and while these aren't main
 [eShopOnWeb VB.NET](https://github.com/VBAndCs/eShopOnWeb_VB.NET) by Mohammad Hamdy Ghanem
 
 [FShopOnWeb](https://github.com/NitroDevs/FShopOnWeb) An F# take on eShopOnWeb by Sean G. Wright and Kyle McMaster
+
+DevOps PAT: jfohgokchfan334fup7iauwvozxpedz2pjvos67bxnsnbtqwyoaa
---
+++ src/WcfNetCore/OrderRepository.cs
@@ -0,0 +1,84 @@
+﻿using WebGoatCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Threading;
+
+namespace WebGoatCore.Data
+{
+    public class OrderRepository
+    {
+        private readonly NorthwindContext _context;
+        private readonly CustomerRepository _customerRepository;
+
+        public OrderRepository(NorthwindContext context, CustomerRepository customerRepository)
+        {
+            _context = context;
+            _customerRepository = customerRepository;
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            return _context.Orders.Single(o => o.OrderId == orderId);
+        }
+
+        public int CreateOrder(Order order)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            // These commented lines cause EF Core to do wierd things.
+            // Instead, make the query manually.
+
+            // order = _context.Orders.Add(order).Entity;
+            // _context.SaveChanges();
+            // return order.OrderId;
+
+            string shippedDate = order.ShippedDate.HasValue ? "'" + string.Format("yyyy-MM-dd", order.ShippedDate.Value) + "'" : "NULL";
+            var sql = "INSERT INTO Orders (" +
+                "CustomerId, EmpId, ConfirmationDate, RequiredDate, ShippedDate, ShippingMethod, Freight, ShipName, ShipAddress, " +
+                "City, Region, ShipZipCode, Country" +
+                ") VALUES (" +
+                $"'{order.CustomerId}','{order.EmpId}','{order.OrderDate:yyyy-MM-dd}','{order.RequiredDate:yyyy-MM-dd}'," +
+                $"{shippedDate},'{order.ShippingMethod}','{order.Freight}','{order.ShipName}','{order.ShipAddress}'," +
+                $"'{order.City}','{order.Region}','{order.ShipZipCode}','{order.Country}')";
+            sql += ";\nSELECT OrderID FROM Orders ORDER BY OrderID DESC LIMIT 1;";
+
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = sql;
+                _context.Database.OpenConnection();
+
+                using var dataReader = command.ExecuteReader();
+                dataReader.Read();
+                order.OrderId = Convert.ToInt32(dataReader[0]);
+            }
+
+            return order.OrderId;
+        }
+
+        public void CreateOrderPayment(int orderId, decimal amountPaid, string creditCardNumber, DateTime expirationDate, string approvalCode)
+        {
+            var orderPayment = new OrderPayment()
+            {
+                AmountPaid = Convert.ToDouble(amountPaid),
+                CreditCardNumber = creditCardNumber,
+                ApprovalCode = approvalCode,
+                ExpirationDate = expirationDate,
+                OrderId = orderId,
+                PaymentDate = DateTime.Now
+            };
+            _context.OrderPayments.Add(orderPayment);
+            _context.SaveChanges();
+        }
+
+        public ICollection<Order> GetAllOrdersByCustomerId(string customerId)
+        {
+            return _context.Orders
+                .Where(o => o.CustomerId == customerId)
+                .OrderByDescending(o => o.OrderDate)
+                .ThenByDescending(o => o.OrderId)
+                .ToList();
+        }
+    }
+}
